df34e0df9321b951b5a9fc67238905d904798826
chetangwl <chetan.deshmukh@galaxyweblinks.in>
2025-11-27 19:06:16 +0530

    files added

---
+++ colors.tsx
@@ -0,0 +1,16 @@
+/**
+ * Name: colors
+ * Desc: Colors value
+ */
+export const colors = {
+  citrusGreen: "#18CFA3",
+  red: "##F22",
+  white: "#ffffff",
+  borderBlack: "rgba(49, 49, 49, 0.08)",
+  borderGrey: "#ebebeb",
+  borderGreyDark: "#ccc",
+  backgroundBlack: "#303030",
+  backgroundGrey: "#979191",
+  backgroundLightGreen: "#DDF3EE",
+  backgroundRed: "rgba(255, 34, 34, 0.12)",
+};
