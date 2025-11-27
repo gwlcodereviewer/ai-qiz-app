dfbef890b47d86c00ab99c900194165689504512
chetangwl <chetan.deshmukh@galaxyweblinks.in>
2025-11-27 19:05:41 +0530

    files added

---
+++ Loader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="futuristic-loader">
+      <div className="ring"></div>
+      <div className="glow"></div>
+    </div>
+  );
+};
+
+export default Loader;
